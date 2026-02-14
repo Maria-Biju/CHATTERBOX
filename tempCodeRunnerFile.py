e66e2efd13e7c6d1224d4ef2367425341b0a281c
Maria-Biju <glennamaria123@gmail.com>
2026-02-14 12:21:06 +0530

    upto admin and history

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from routes.user_routes import router as user_router
+from routes.websocket_routes import router as websocket_router
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.include_router(user_router)
+app.include_router(websocket_router)
+
+app.mount("/ui", StaticFiles(directory="frontend", html=True), name="frontend")
+
+@app.get("/")
+def home():
+    return {"message": "Chat server running"}
